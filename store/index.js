ede22a40a1e7ec36ff0bbba4b512cbce1e2aa078
Jonathan White <mrwhite@Jonathans-MacBook-Pro-2.local>
2016-04-13 12:08:30 -0400

    added state debugger

--- store/index.js
+++ store/index.js
@@ -1,17 +1,31 @@
 'use strict'
 
+/**
+ * ## Store
+ * temporary state store for information about
+ * users that the bot is interfacing with
+ */
 let store = {
-  // hashtable of users
-  users: {},
+  _users: {},
 
   getState: function(hash) {
-    return user[hash]
+    if (!this._users[hash]) return []
+
+    return this._users[hash]
   },
 
   push: function(hash, data) {
-    if (!user[hash]) user[hash] = []
+    if (!this._users[hash]) this._users[hash] = []
 
-    user[hash].push(data)
+    this._users[hash].push(data)
+  },
+
+  peek: function(hash) {
+    if (!this._users[hash]) return []
+
+    const top = this._users[hash].length - 1
+
+    return this._users[hash][top]
   }
 }
 
